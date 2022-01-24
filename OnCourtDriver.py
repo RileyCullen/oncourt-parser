847333915b3257f928ddcbc2941bada76a6c982b
Riley Cullen <cullenriley@seattleu.edu>
2022-01-24 15:42:48 -0800

    Documentation added to main

--- OnCourtDriver.py
+++ OnCourtDriver.py
@@ -1,4 +1,9 @@
 def main():
+    """
+    The entry point for the OnCourt data parser, this function is responsible 
+    for verifying the input arguments, in addition to opening the files and 
+    calling the respective parser. 
+    """
     pass
 
 if __name__ == "__main__":
